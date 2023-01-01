e0783eb0ebc8c87a79ea7ff0e51eca0fe672ed7a
Ayomide <heritageayomide2017@gmail.com>
2023-01-01 12:17:50 +0100

    placed secret key in secure file

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    RAPID_API_KEY:'d1a1d608d6mshaaa12402f8d98b0p165bc3jsn0cb15378174a',
-    RAPID_API_HOST:'genius-song-lyrics1.p.rapidapi.com',
-  },
 }
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -49,8 +49,8 @@ const fetchSongRecommendations = async () => {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': process.env.RAPID_API_KEY as string,
-      'X-RapidAPI-Host': process.env.RAPID_API_HOST as string
+      'X-RapidAPI-Key': process.env.NEXT_PULIC_RAPID_API_KEY as string,
+      'X-RapidAPI-Host': process.env.NEXT_PULIC_RAPID_API_HOST as string
     }
   };
   const res = await fetch('https://genius-song-lyrics1.p.rapidapi.com/song/recommendations/?id=2396871', options)
@@ -64,7 +64,7 @@ export default function Home() {
 
   // queries
   const { data: recommendation, status } = useQuery('recommendations', fetchSongRecommendations);
-  // console.log(recommendation);
+  console.log(recommendation);
 
   const [openNav, setOpenNav] = useState(false);
   const HandleClick = () => {
